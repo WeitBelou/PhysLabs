6dbc780f0dc03784f0991f09d71c644a96e59347
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-02-17 15:21:40 +0300

    Update plotting script.

--- 9.1/plot.py
+++ 9.1/plot.py
@@ -26,8 +26,8 @@ def _setup_axes(plotter: Callable[[Any], plt.Axes]) -> Callable[[Any], None]:
 def plot_freq(T: np.ndarray, f_0: np.ndarray, f: np.ndarray):
     data = pd.DataFrame()
     T_label = '$T$, K'
-    f_0_label = '$f_0$, Гц'
-    f_label = '$f$, Гц'
+    f_0_label = '$f_0$, кГц'
+    f_label = '$f$, кГц'
 
     data[T_label] = T
     data[f_0_label] = f_0
@@ -35,9 +35,6 @@ def plot_freq(T: np.ndarray, f_0: np.ndarray, f: np.ndarray):
 
     ax = data.plot.scatter(x=T_label, y=f_0_label, c='red')
     data.plot.scatter(ax=ax, x=T_label, y=f_label, c='blue')
-
-    ax.set_xlim(0, None)
-    ax.set_ylim(0, None)
 
     ax.set_title('Зависимость $f, f_0$ от $T$.')
 
@@ -48,15 +45,12 @@ def plot_freq(T: np.ndarray, f_0: np.ndarray, f: np.ndarray):
 def plot_kappa(T: np.array, f_0: np.array, f: np.array):
     data = pd.DataFrame()
     T_label = '$T$, K'
-    kappa_label = '$\\kappa$'
+    kappa_label = '$\\frac{1}{\\kappa}$'
 
     data[T_label] = T
     data[kappa_label] = f ** 2 / (f_0 ** 2 - f ** 2)
 
     ax = data.plot.scatter(x=T_label, y=kappa_label, c='red')
-
-    ax.set_xlim(0, None)
-    ax.set_ylim(0, None)
 
     ax.set_title('Зависимость $\\frac{f^2}{f_0^2 - f^2}$ от $T$.')
 
@@ -65,7 +59,8 @@ def plot_kappa(T: np.array, f_0: np.array, f: np.array):
 
 def _compute_temperature(V: np.array):
     t_0 = 21 + 273
-    return V / 41 + t_0
+    delta_V = 90
+    return (V - delta_V) / 41 + t_0
 
 
 def main():
