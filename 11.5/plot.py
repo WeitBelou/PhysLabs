8e1d8fb7173d22e498a7e51328f23e459c435d5a
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-04-06 18:14:17 +0300

    Add grid and labels.

--- 11.5/plot.py
+++ 11.5/plot.py
@@ -4,7 +4,25 @@ import matplotlib.pyplot as plt
 
 def main():
     data = pd.read_csv('./data/volt-amper.csv')
-    data.plot.scatter(x='V', y='I')
+
+    R_1 = 630
+    R_2 = 630
+    R_3 = 630
+
+    K = (R_1 + 2 * (R_2 + R_3)) / ((R_1 + 2 * R_2) * R_3)
+
+    data['I'] = data['I'] * K * 1e3
+
+    ax = data.plot.scatter(x='V', y='I')
+
+    ax.set_xlim(0)
+    ax.set_ylim(0)
+
+    ax.set_xlabel(r"V, В")
+    ax.set_ylabel(r"I, мА")
+
+    ax.grid()
+
     plt.show()
 
 
